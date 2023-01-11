69d1bb2b3dd9a2c978b9108085b39fe05c0ac5b7
karanveersinghA2XYY <121312976+karanveersinghA2XYY@users.noreply.github.com>
2023-01-11 20:58:36 +0530

    Update alive.py
    Signed-off-by: karanveersinghA2XYY <121312976+karanveersinghA2XYY@users.noreply.github.com>

--- Himawari/modules/alive.py
+++ Himawari/modules/alive.py
@@ -29,10 +29,10 @@ HIMAWARI = "https://telegra.ph/file/9dfcdab5244a61b323210.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   STB = event.sender.first_name
-  STB = f"**I m {BOT_NAME}** \n\n" + "**I'm Working Properly**\n\n"
+  STB = f"**I am {BOT_NAME}** \n\n" + "**I am Working Properly**\n\n"
   STB += "**Python Version : 3.10.6**\n\n"
   STB += "**python-Telegram-Bot : 13.12**\n\n"
-  BUTTON = [[Button.url("Support", "https://t.me/{SUPPORT_CHAT}"), Button.url("Updates", "https://t.me/{UPDATES_CHANNEL}")]]
+  BUTTON = [[Button.url("Support", "https://t.me/ENGLISH_SPEAKERSSS"), Button.url("Updates", "https://t.me/Baka_forum")]]
   await tbot.send_file(event.chat_id, HIMAWARI, caption=STB,  buttons=BUTTON)
 
   # thanks to stb the gay
